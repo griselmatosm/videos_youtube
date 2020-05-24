3c7219e07a3eb526520717c6008b019ac69eeded
Grisel Matos <griselmatosm@gmail.com>
2020-05-25 01:05:39 +0200

    Add VideoDetail component. Add default video

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,45 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import dataApi from '../services/getDataFromApi';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount(){
+    this.onTermSubmit('people are awesome')
+  }
+
   onTermSubmit = async (term) => {
     const response = await dataApi.get('/search', {
       params: {
         q: term,
       },
     });
-    this.setState({videos: response.data.items});
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]    
+    });
   };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
-    console.log(this.state.videos);
-    
     return (
       <div className="ui container">
         <SearchBar onSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
